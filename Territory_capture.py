40da0495f372e55b11601a5a417a31804a96b1af
Lelelr <rea.leandre@yahoo.com>
2025-09-23 12:17:56 +0200

    resolve problem 3

--- Territory_capture.py
+++ Territory_capture.py
@@ -1,46 +1,43 @@
 import pygame as pg, sys
 
-# ------------ CONFIG ------------
-
+# ------------ CONFIG ------------ #
 pg.init()
-WIDTH, HEIGHT, FPS = 800, 480, 60
-screen = pg.display.set_mode((WIDTH, HEIGHT))
+WIDTH, HEIGHT, FPS = 1500, 1000, 60
+
+# Ecran Fullscreen
+screen = pg.display.set_mode(((0, 0)), pg.FULLSCREEN)
+
 clock = pg.time.Clock()
 
 
-# ------------ CHARGEMENT IMAGE ------------
+# ---------- Bouton Quitter ----- #
+# Couleurs et police du bouton
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+font = pg.font.SysFont(None, 30)
+
+# Position bas gauche
+button_rect = pg.Rect(10, HEIGHT - 50, 120, 40)
 
 
-# ------------ CREER MAP ------------
-MCOLORS = (200,80,80)
-
-maps = [
-    pg.Rect(150, 80, 20, 20)
-]
-
-
-# ------------ BOUCLE PRINCIPALE ------------
-
-running = True
 while running:
+
     dt = clock.tick(FPS) / 1000.0
+
     for e in pg.event.get():
         if e.type == pg.QUIT:
             running == False
 
-
-
-# ------------ DESSIN ------------
+        elif e.type == pg.MOUSEBUTTONDOWN:
+            if button_rect.collidepoint(e.pos):
+                running = False
 
     screen.fill((24,26,32))
 
-    for m in maps:
-        pg.draw.rect(screen,MCOLORS, m)
 
-
-
-
-
+    pg.draw.rect(screen, BLACK, button_rect)
+    text_surf = font.render("Quitter", True, WHITE)
+    screen.blit(text_surf, (button_rect.x + 10, button_rect.y + 5))
 
 
 
