e1716558e5a55228df4b7704166fd1cdffaf70aa
Nicolas <gagnantnicolas64@gmail.com>
2025-09-23 11:48:12 +0200

    first merge generate_map on map

--- Territory_capture.py
+++ Territory_capture.py
@@ -1,17 +1,49 @@
 import pygame as pg, sys
+
+# ------------ CONFIG ------------
+
 pg.init()
 WIDTH, HEIGHT, FPS = 800, 480, 60
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
-running = True
 
+
+# ------------ CHARGEMENT IMAGE ------------
+
+
+# ------------ CREER MAP ------------
+MCOLORS = (200,80,80)
+
+maps = [
+    pg.Rect(150, 80, 20, 20)
+]
+
+
+# ------------ BOUCLE PRINCIPALE ------------
+
+running = True
 while running:
     dt = clock.tick(FPS) / 1000.0
     for e in pg.event.get():
         if e.type == pg.QUIT:
             running == False
 
+
+
+# ------------ DESSIN ------------
+
     screen.fill((24,26,32))
+
+    for m in maps:
+        pg.draw.rect(screen,MCOLORS, m)
+
+
+
+
+
+
+
+
     pg.display.flip()
 
-pg.quit(); sys.exit()
+pg.quit(); sys.exit()  # QUITTEZ LE JEU
