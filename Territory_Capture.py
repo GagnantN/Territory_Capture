eb3725d49c3531f4d3b484deff0ccdac2f490966
Nicolas <gagnantnicolas64@gmail.com>
2025-09-24 15:39:52 +0200

    Map and spawn player check

--- Territory_Capture.py
+++ Territory_Capture.py
@@ -107,7 +107,7 @@ def page_accueil():
 # ------------------------ JEU -------------------------- #
 def jeu():
     # Maps
-    BCOLOR, case_original = create_map(screen)
+    BCOLOR, case_original, pos_joueur_1, pos_joueur_2 = create_map(screen)
     running = True
     menu_actif = False
     retour_menu = False # Revenir au menu principal
@@ -155,6 +155,20 @@ def jeu():
         for rect, couleur in case_original :
             pg.draw.rect(screen, couleur, rect)  # remplissage
             pg.draw.rect(screen, BCOLOR, rect, 1) # bordure
+            
+        # Dessiner les joueurs
+        if pos_joueur_1:
+            i, j = pos_joueur_1
+            x = int(largeur * 0.25) + j * (rect.width)
+            y = int(hauteur * 0.1) + i * (rect.height)
+            pg.draw.rect(screen, (70, 130, 180), (x, y, rect.width, rect.height))  # joueur_01
+
+        if pos_joueur_2:
+            i, j = pos_joueur_2
+            x = int(largeur * 0.25) + j * (rect.width)
+            y = int(hauteur * 0.1) + i * (rect.height)
+            pg.draw.rect(screen, (178, 34, 3), (x, y, rect.width, rect.height))  # joueur_02
+
 
 
         # Bouton Menu en jeu
